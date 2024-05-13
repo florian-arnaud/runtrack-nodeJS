a13ddaad66c1d2ec6b5ae69b919a22c4c2ae0d1b
florian-arnaud <florian.arnaud@laplateforme.io>
2024-05-13 09:41:27 +0200

    Initial commit - Job 1 to 3

---
+++ job2/index.js
@@ -0,0 +1,8 @@
+/* 
+Créez une variable nommée “count” et attribuez-lui la valeur 3. Utilisez un
+seul console.log() pour afficher dans le terminal : “La valeur de count est : 3”.
+Exécutez votre script pour vous assurer que tout fonctionne comme prévu.
+*/
+let a = 8;
+let b = 12;
+console.log( a + b)
---
+++ job3/index.js
@@ -0,0 +1,8 @@
+/* 
+Créez une variable nommée “count” et attribuez-lui la valeur 3. Utilisez un
+seul console.log() pour afficher dans le terminal : “La valeur de count est : 3”.
+Exécutez votre script pour vous assurer que tout fonctionne comme prévu.
+*/
+
+let count = 3
+console.log("La valeur de count est: " = count);
